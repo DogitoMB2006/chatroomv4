5a1a2a3a4a08e238b36199e88b8596ad77de4adb
DogitoMB2006 <jesuscorcino30@gmail.com>
2025-06-03 16:19:16 -0400

    g

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center">Página de Login</h1>
+       
         <Login onLogin={handleLoginSuccess} />
       </div>
     </div>
